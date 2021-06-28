0aa5d0a8adeeb9c14147982372e89c455aaa5470
Gregoire <g.guille@thirard.fr>
2021-06-28 14:19:35 +0200

    creation de l'application dans un autr fichier

--- server/app.py
+++ server/app.py
@@ -7,38 +7,28 @@ from datetime import datetime
 import json
 import re
 # import modéle
-from update.models import db, Server, Update
+from database.models import db
+
+# import routes
 from update.routes import updateB
+from admin.routes import adminB
+
 # app config
-app = Flask(__name__, template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///updates.sqlite3'
-app.config['SECRET_KEY'] = "zdzhdhzdh"
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+from createApp import app
 
-
+# Blueprint
 app.register_blueprint(updateB)
+app.register_blueprint(adminB)
 
-# db
+# Database
 db.app = app
 db.init_app(app)
 db.create_all()
 
-# admin view
-# admin = Admin(app, name='Updates', template_mode='bootstrap3')
-# admin.add_view(ModelView(Server, db.session))
-# admin.add_view(ModelView(Update, db.session))
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return 'None'
-
-
-@app.route("/test", methods=['GET', 'POST'])
-def test():
-    server = Server(name="SRVT2WP", ip="10.33.1.221")
-    db.session.add(server)
-    db.session.commit()
-    return "ok"
 
 
 @app.route("/dashboard", methods=['GET', 'POST'])
