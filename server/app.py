0e0a8ca55a0ab83f8d1b3b4e90cb50713a533680
Gregoire <g.guille@thirard.fr>
2021-06-29 16:07:23 +0200

    ajout bootstrap pour génération auto des formulaires

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_bootstrap import Bootstrap
 from datetime import datetime
 import json
 import re
@@ -15,6 +16,7 @@ from admin.routes import adminB
 
 # app config
 from createApp import app
+Bootstrap(app)
 
 # Blueprint
 app.register_blueprint(updateB)
