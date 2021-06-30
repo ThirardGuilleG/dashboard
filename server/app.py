219a8f688bdf2eafb7d160c8b7783151525636ca
Gregoire <g.guille@thirard.fr>
2021-06-30 10:43:01 +0200

    envoi de la liste des serveurs en POST avec un token ajout flask_migrate pour les futurs modification de BDD

--- server/app.py
+++ server/app.py
@@ -1,14 +1,14 @@
-from os import name
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from datetime import datetime
 import json
-import re
+import re, base64
 # import modéle
-from database.models import db
+from database.models import db, Server
 
 # import routes
 from update.routes import updateB
@@ -27,6 +27,8 @@ db.app = app
 db.init_app(app)
 db.create_all()
 
+migrate = Migrate(app, db)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -37,6 +39,16 @@ def index():
 def dashboard():
     return render_template('dashboard.html')
 
+
+@app.post('/servers')
+def servers():
+    args= request.form
+    token = args.get('token')
+    goodToken = "5i3#&N4.r`ftp~s/CG:?t7tCq}zE#5g4Xf58m7.t"
+    if token == goodToken:
+        servers = [(server.name,server.ip) for server in Server.query.all()]
+        return jsonify(servers)
+    abort(403)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
