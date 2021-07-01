39eb6494f1354b71c198aa893c65f89c3911e78b
Gregoire <g.guille@thirard.fr>
2021-07-01 10:21:45 +0200

    class user pour la connection utilisateur

--- server/database/models.py
+++ server/database/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import backref
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import enum
 db = SQLAlchemy()
 
 
@@ -22,7 +23,6 @@ class Update(db.Model):
     maxSize = db.Column(db.Integer)
     date = db.Column(db.DateTime())
     url = db.Column(db.String(100))
-    # updates = db.relationship('Server', secondary=updates, lazy='subquery', backref=db.backref('servers', lazy=True))
     servers = db.relationship("UpdateAssociation", back_populates="update")
 
 
@@ -31,8 +31,39 @@ class Server(db.Model):
     name = db.Column(db.String(75), unique=True, nullable=False)
     ip = db.Column(db.String(16), unique=True, nullable=False)
     version = db.Column(db.String(50))
+    description = db.Column(db.String(250))
     updates = db.relationship("UpdateAssociation", back_populates="server")
 
+
+class UserType(enum.Enum):
+    admin = 1
+    update = 2
+    server = 3
+    regular = 4
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, 
+        primary_key=True
+        )
+    firstname = db.Column(db.String(75))
+    lastname = db.Column(db.String(75))
+    username = db.Column(db.String(75), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.Integer, nullable=False)
+    creation_date = db.Column(db.DateTime) 
+    last_connection_date = db.Column(db.DateTime) 
+
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(
+            password,
+            method='sha256'
+        )
+    
+    def check_password(self, password):
+        return check_password_hash(self.password,password)
 
 
 def get_db() -> SQLAlchemy:
