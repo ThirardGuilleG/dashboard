e65a3182423a650a6787766ccab538dfcd671363
Gregoire <g.guille@thirard.fr>
2021-07-02 17:03:48 +0200

    Amélioration du fetch des updates et historique des updates Ajout d'une régle dans le firewall pour faire fonctionner

--- checkUpdate.ps1
+++ checkUpdate.ps1
@@ -1,27 +1,22 @@
 
 function addRuleForPSUpdate($ServerName){
     # PSWindowsUpdate à besoin du port 143 en TCP et de la DLL dllhost(System32)
-
+    # TODO ajouter la régle si existe pas
     Write-Host "création route pour : $($ServerName)"
     $session = New-CimSession -ComputerName $ServerName
-    New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate TCP" -Direction Inbound -LocalPort 143 -Protocol TCP -Action Allow -CimSession $session
-    New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate DLL" -Direction Inbound -Program "%SystemRoot%\System32\dllhost.exe" -Action Allow -CimSession $session
+    New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate TCP" -Direction Inbound -LocalPort 143 -Protocol TCP -Action Allow -Profile Domain -CimSession $session
+    New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate DLL" -Direction Inbound -Program "%SystemRoot%\System32\dllhost.exe" -Action Allow -Profile Domain -CimSession $session
 }
 
 function betterFetchUpdates($nameServer){
     $updates = Get-WindowsUpdate -ComputerName $nameServer 
-    return $updates | Select-Object Size,Status,ComputerName,KB,Title,Description,Deadline,IsDownloaded,IsInstalled,MoreInfoUrls,RebootRequired
+    return $updates | Select-Object Size,Status,ComputerName,KB,Title,Description,Deadline,IsDownloaded,IsInstalled,MoreInfoUrls,RebootRequired,LastDeploymentChangeTime
 }
 
-
-function fetchUpdates($pNameServer){
-    $r =Invoke-Command -ComputerName $pNameServer -ScriptBlock {
-        $UpdateSession = New-Object -ComObject Microsoft.Update.Session
-        $UpdateSearcher = $UpdateSession.CreateupdateSearcher()
-        $Updates = @($UpdateSearcher.Search("IsHidden=0 and IsInstalled=0").Updates)
-        $Updates 
-    }
-    return $r
+function getHistory($name, $NumberDayToFetch){
+    $yesterday = Get-Date
+    $yesterday = $yesterday.AddDays(-31).ToString("yyyy-MM-dd")
+    return Get-WUHistory -ComputerName $Name -MaxDate $yesterday | Select-Object ComputerName, OperationName, Date, Title, Result, KB
 }
 
 function getServers(){
@@ -33,14 +28,14 @@ function getServers(){
 }
 
 function main(){
-    $yesterday = Get-Date
-    $yesterday.AddDays(-1).ToString("yyyy-MM-dd")
+    
     foreach($server in (getServers | ConvertFrom-Json)){
         $Name,$ip = $server.value
+        addRuleForPSUpdate($Name)
         Write-Host("Fetch update pour $($Name) ip : $($ip)")
         $result = betterFetchUpdates($Name)
         $needRestart = Get-WURebootStatus -ComputerName $Name -Silent
-        $historyUpdate = Get-WUHistory -ComputerName $Name -MaxDate $yesterday
+        $historyUpdate = getHistory($Name,1)
         $toSend = @{'updates'=$result ;
         'history'=$historyUpdate;
         "needRestart"=$needRestart}
