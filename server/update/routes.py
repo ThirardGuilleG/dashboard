d8284ebaec07364b68ad4d6c593510e2e4df0b0f
Gregoire <g.guille@thirard.fr>
2021-07-02 17:04:58 +0200

    Utilisation de fonction pour ajout d'update et passage à done

--- server/update/routes.py
+++ server/update/routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify, render_template
-from datetime import datetime
-import json, re
-from flask_sqlalchemy import Pagination
-from sqlalchemy import and_, or_
+import json
+from sqlalchemy import and_
 from loguru import logger
 from database.models import Server, Update, get_db, UpdateAssociation
+from update.utils import addUpdates, doneUpdates
+
 
 updateB = Blueprint("update",__name__, url_prefix="/update")
 db = get_db()
@@ -19,59 +19,9 @@ def data():
     resp = json.loads(answer.decode())
     # TODO Fonction
     if resp:
-        for update in resp.get('updates'):
-            logger.debug(update)
-            logger.debug(resp.get('needRestart'))
-            title = update.get('Title')
-            description = update.get('Description')
-            server = update.get('ComputerName')
-            size = update.get('Size')
-            kb = update.get('KB')
-            status = update.get('Status')
-            reboot = update.get('RebootRequired')
-            installed = update.get('IsInstalled')
-            downloaded = update.get('IsDownloaded')
-            url = update.get('MoreInfoUrls')
-            logger.info(f'Mise à jour récupérée pour : {server}')
-            # Server
-            myServer = Server.query.filter_by(name=server).first()
-            if not myServer:
-                logger.warning("le serveur n'est pas en bdd")
-                return jsonify("erreur",200)
-            myUpdate = Update.query.filter(or_(Update.kb==kb,Update.title==title)).first()
-
-            # Création de la MAJ si elle existe pas
-            if not myUpdate:
-                logger.debug(f"Création de la MAJ : {kb}")
-                newUpdate = Update(kb=kb,title=title,date=datetime.now(),size=size,infoUrl=url,description=description)
-                myUpdate = newUpdate
-
-            modifyinformation = UpdateAssociation.query.filter_by(idUpdate=myUpdate.id,idServer=myServer.id).first()
-            # Regarde la chaine so
-            if modifyinformation:
-                # mise à jour des informations
-                logger.debug(f"L'association entre la MAJ {modifyinformation} existe déjà modification des informations")
-                modifyinformation.installed = installed
-                modifyinformation.downloaded = downloaded
-                modifyinformation.rebootRequired = reboot
-                modifyinformation.date = datetime.now()
-                db.session.add(modifyinformation)
-            else:
-                logger.debug(f"Création de l'association pour la MAJ {myUpdate} et server : {server}")
-                with db.session.no_autoflush:
-                    # Création de l'association
-                    # ajout des données en plus
-                    association = UpdateAssociation(done=False,downloaded=downloaded, rebootrequired=reboot,date=datetime.now(), installed=installed) 
-                    # Ajout du server relié à l'update
-                    db.session.no_autoflush
-                    association.update = myUpdate
-                    # ajout en bdd
-                    # myUpdate.servers.append(association)
-                    # db.session.add(myUpdate)
-                    myServer.updates.append(association)
-                    db.session.add(myServer)
-            db.session.commit()
-        return jsonify("ok", 201)
+        statut = addUpdates(resp.get('updates'))
+        doneUpdates(resp.get('history'))
+        return jsonify(statut)
 
 
 @updateB.route('/<int:idServer>')
@@ -79,25 +29,26 @@ def update(idServer):
     names = ['Titre mise à jour', "Taille Max(Mo)", "Demande un redémarrage", "Installé", "Lien + information", "date de sortie", "dernière MAJ"]
     # updates = UpdateAssociation.query.filter_by(idServer=idServeur).all()
     updates = db.session.query(UpdateAssociation).filter(and_(UpdateAssociation.idServer==idServer,UpdateAssociation.done==False)).join(Update, UpdateAssociation.update).all()
+    done = db.session.query(UpdateAssociation).filter(and_(UpdateAssociation.idServer==idServer,UpdateAssociation.done==True)).join(Update, UpdateAssociation.update).all()
+    doneUpdates = [ (update.update.title, update.update.kb, update.date) for update in done]
     rows = []
     ids = [ (update.idServer,update.idUpdate) for update in updates]
     for row in updates:
         update = row.update
-        rows.append([update.title, update.maxSize/1000000, row.rebootrequired, row.installed,update.url ,update.date, row.date])
-        # update = Update.query.get(row.id)
-        # if update:
-        #     information = [update.title, update.maxSize, row.rebootrequired, row.installed, update.date]
+        rows.append([update.title, update.size, row.rebootrequired, row.installed,update.infoUrl ,update.date, row.date])
+        
     myServer = Server.query.get_or_404(idServer)
-    return render_template('update/view.html', title= f"{myServer.name}",description=f"Voici toutes les mises à jours pour : {myServer.name}", names=names, rows=rows,ids=ids)
+    return render_template('update/view.html', title= f"{myServer.name}",description=f"Voici toutes les mises à jours pour : {myServer.name}", 
+            names=names, rows=rows, ids=ids, doneUpdates=doneUpdates)
 
 
 @updateB.get('/last', defaults={'page':1})
 @updateB.get('/last/<int:page>')
 def last_update(page):
     names = ['Titre mise à jour', "serveur", "date de sortie", "dernière MAJ"]
-    updates = db.session.query(UpdateAssociation).filter(UpdateAssociation.done==False).join(Update, UpdateAssociation.update).paginate(page=page,per_page=10)
+    updates = db.session.query(UpdateAssociation).filter(UpdateAssociation.done==False).join(Update, UpdateAssociation.update).paginate(page=page,per_page=1000)
     ids = [(update.idServer,update.idUpdate) for update in updates.items]
-    rows = [(row.update.title, "test" ,row.update.date, row.date) for row in updates.items]
+    rows = [(row.update.title, row.server.name ,row.update.date, row.date) for row in updates.items]
     return render_template('update/tables.html', title= "Dernière mise a jours", names=names, rows=rows,ids=ids, pagination=updates,endpoint='update.last_update')
 
 
--- server/update/utils.py
+++ server/update/utils.py
@@ -1,8 +1,98 @@
-from database.models import Update
+from typing import Tuple
+from database.models import Update,Server,UpdateAssociation, get_db
+from sqlalchemy import and_, or_
+from datetime import datetime
+from loguru import logger
 
-def addUpdates():
-    pass
+import enum
+
+db = get_db()
+
+class Etat(enum.Enum):
+    Succeeded = True
+    Failed = False
+    Aborted = False
+    InProgress = False
 
 
-def doneUpdates():
-    pass
+def doneUpdates(history_updates: list):
+    logger.info("check des mise à jours pour :")
+    for update in history_updates:
+        server = update.get('ComputerName')
+        kb = update.get('KB')
+        title = update.get('Title')
+        date = datetime.fromtimestamp(int(update.get('Date')[6:16]))
+        result = Etat[update.get('Result').strip()]
+        if result.value is True:
+            s = Server.query.filter_by(name=server).first()
+            update = Update.query.filter(or_(Update.kb==kb,Update.title==title)).first()
+            if update:
+                uptade_to_valid = UpdateAssociation.query.filter(and_(UpdateAssociation.idUpdate==update.id,
+                                                                UpdateAssociation.idServer==s.id), UpdateAssociation.done==False).first()
+                # Update des informations
+                if uptade_to_valid:
+                    logger.info(f"La mise à jours : {uptade_to_valid.idUpdate} est ok pour : {server}")
+                    uptade_to_valid.done=True
+                    uptade_to_valid.date=date
+                    uptade_to_valid.installed=True
+                    uptade_to_valid.downloaded=True
+                    uptade_to_valid.rebootrequired=False
+                    db.session.add(uptade_to_valid)
+                    db.session.commit()
+
+
+def addUpdates(pUpdates: list):
+    for update in pUpdates:
+            title = update.get('Title').encode('utf-8')
+            description = update.get('Description').encode('utf-8')
+            server = update.get('ComputerName')
+            size = update.get('Size')
+            kb = update.get('KB')
+            status = update.get('Status')
+            reboot = update.get('RebootRequired')
+            installed = update.get('IsInstalled')
+            downloaded = update.get('IsDownloaded')
+            url = update.get('MoreInfoUrls')
+            deployementDate = datetime.fromtimestamp(int(update.get('LastDeploymentChangeTime')[6:16]))
+            logger.debug(deployementDate)
+            logger.info(f'Mise à jour récupérée pour : {server}')
+            # Server
+            myServer = Server.query.filter_by(name=server).first()
+            if not myServer:
+                logger.warning("le serveur n'est pas en bdd")
+                return ('error',202)
+            myUpdate = Update.query.filter(or_(Update.kb==kb,Update.title==title)).first()
+
+            # Création de la MAJ si elle existe pas
+            if not myUpdate:
+                logger.debug(f"Création de la MAJ : {kb}")
+                newUpdate = Update(kb=kb,title=title,date=deployementDate,size=size,infoUrl=url,description=description)
+                myUpdate = newUpdate
+
+            modifyinformation = UpdateAssociation.query.filter_by(idUpdate=myUpdate.id,idServer=myServer.id).first()
+            # Regarde la chaine so
+            if modifyinformation:
+                # mise à jour des informations
+                logger.debug(f"L'association entre la MAJ {modifyinformation} existe déjà modification des informations")
+                modifyinformation.installed = installed
+                modifyinformation.downloaded = downloaded
+                modifyinformation.rebootRequired = reboot
+                modifyinformation.date = datetime.now()
+                db.session.add(modifyinformation)
+            else:
+                logger.debug(f"Création de l'association pour la MAJ {myUpdate} et server : {server}")
+                with db.session.no_autoflush:
+                    # Création de l'association
+                    # ajout des données en plus
+                    association = UpdateAssociation(done=False,downloaded=downloaded, rebootrequired=reboot,date=datetime.now(), installed=installed) 
+                    # Ajout du server relié à l'update
+                    db.session.no_autoflush
+                    association.update = myUpdate
+                    # ajout en bdd
+                    # myUpdate.servers.append(association)
+                    # db.session.add(myUpdate)
+                    myServer.updates.append(association)
+                    db.session.add(myServer)
+            db.session.commit()
+
+
