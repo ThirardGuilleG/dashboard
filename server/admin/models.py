3979d7400a24663f110d9c72e0fd3485c18175af
Gregoire <g.guille@thirard.fr>
2021-07-02 17:04:00 +0200

    ajout de la description

--- server/admin/models.py
+++ server/admin/models.py
@@ -1,10 +1,11 @@
 from wtforms.fields.core import IntegerField
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired, IPAddress, InputRequired
 
 class ServerForm(FlaskForm):
     name = StringField('Nom', validators=[DataRequired(),InputRequired("Veuillez remplir ce champ")])
     ip = StringField("IP", validators=[DataRequired(),IPAddress()])
     version = IntegerField('Version', validators=[DataRequired(),InputRequired("Veuillez remplir ce champ")])
+    description = TextAreaField('Description', validators=[DataRequired(),InputRequired("Veuillez remplir ce champ")])
     submit = SubmitField(label="Envoyer")
