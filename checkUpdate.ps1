80e5da9f31ed5566c3cd15b60d6af61f03871839
Gregoire <g.guille@thirard.fr>
2021-07-05 14:32:14 +0200

    ajout des régles pour le firewall automatiquement

--- checkUpdate.ps1
+++ checkUpdate.ps1
@@ -2,11 +2,17 @@
 function addRuleForPSUpdate($ServerName){
     # PSWindowsUpdate à besoin du port 143 en TCP et de la DLL dllhost(System32)
     # TODO ajouter la régle si existe pas
-    Write-Host "création route pour : $($ServerName)"
+    
     $session = New-CimSession -ComputerName $ServerName
     # Get-NetFirewallRule -DisplayName <String[]>
-    New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate TCP" -Direction Inbound -LocalPort 143 -Protocol TCP -Action Allow -Profile Domain -CimSession $session
-    New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate DLL" -Direction Inbound -Program "%SystemRoot%\System32\dllhost.exe" -Action Allow -Profile Domain -CimSession $session
+    if (-Not (Get-NetFirewallRule -DisplayName "Allow PSWindowsUpdate TCP" -CimSession $session)){
+        Write-Host "création route TCP pour : $($ServerName)"
+        New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate TCP" -Direction Inbound -LocalPort 143 -Protocol TCP -Action Allow -Profile Domain -CimSession $session
+    }
+    if (-Not (Get-NetFirewallRule -DisplayName "Allow PSWindowsUpdate TCP" -CimSession $session)){
+        Write-Host "création route DLL pour : $($ServerName)"
+        New-NetFirewallRule -DisplayName "Allow PSWindowsUpdate DLL" -Direction Inbound -Program "%SystemRoot%\System32\dllhost.exe" -Action Allow -Profile Domain -CimSession $session
+    }
 }
 
 function betterFetchUpdates($nameServer){
@@ -16,6 +22,7 @@ function betterFetchUpdates($nameServer){
 
 function getHistory($name, $NumberDayToFetch){
     $yesterday = Get-Date
+    # TODO changer à -5
     $yesterday = $yesterday.AddDays(-31).ToString("yyyy-MM-dd")
     return Get-WUHistory -ComputerName $Name -MaxDate $yesterday | Select-Object ComputerName, OperationName, Date, Title, Result, KB
 }
@@ -32,7 +39,7 @@ function main(){
     
     foreach($server in (getServers | ConvertFrom-Json)){
         $Name,$ip = $server.value
-        # addRuleForPSUpdate($Name)
+        addRuleForPSUpdate($Name)
         Write-Host("Fetch update pour $($Name) ip : $($ip)")
         $result = betterFetchUpdates($Name)
         $needRestart = Get-WURebootStatus -ComputerName $Name -Silent
