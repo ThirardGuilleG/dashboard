51de37cbf4e3686e3c920c13671ab90b822aeda7
Gregoire <g.guille@thirard.fr>
2021-07-16 14:54:46 +0200

    clear du code et fonction pour test le check_services

--- main.ps1
+++ main.ps1
@@ -1,6 +1,7 @@
 ."$PSScriptRoot\check_services.ps1"
 ."$PSScriptRoot\checkUpdate.ps1"
-
+$server_url = "http://127.0.0.1:5000"
+$token = '5i3#&N4.r`ftp~s/CG:?t7tCq}zE#5g4Xf58m7.t'
 enum Server_Version{
     WindowsServer19 = 19
     WindowsServer16 = 16
@@ -13,10 +14,22 @@ enum Server_Version{
 function getServers(){
     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
     $headers.Add("Content-Type", "multipart/form-data")
-    $postParams = @{token='5i3#&N4.r`ftp~s/CG:?t7tCq}zE#5g4Xf58m7.t'}
-    $response = Invoke-RestMethod -Uri 'http://127.0.0.1:5000/servers' -Method Post -Body $postParams;
+    $postParams = @{token=$token}
+    $response = Invoke-RestMethod -Uri "$server_url/servers" -Method Post -Body $postParams;
     return $response | ConvertTo-Json
 }
+
+
+function check_services(){
+    foreach($server in (getServers | ConvertFrom-Json)){
+        $Name,$ip = $server.value
+        $etat = launch_check $Name
+        $services = @{'server'=$Name; 'etat'=$etat}
+        $response = Invoke-RestMethod "$server_url/admin/data" -Method 'POST' -Body ($services | ConvertTo-Json) -ContentType "application/json";
+        Write-Host $response -ForeGroundColor Green
+    }
+}
+
 
 function main(){
     foreach($server in (getServers | ConvertFrom-Json)){
@@ -25,24 +38,24 @@ function main(){
         addRuleForPSUpdate($Name)
         # Check les MAJ.
         Write-Host("Fetch update pour $($Name) ip : $($ip)")
-        # $result = fetch_updates($Name)
+        $result = fetch_updates($Name)
         # Besoin de restart le serveur.
-        # $needRestart = Get-WURebootStatus -ComputerName $Name -Silent
+        $needRestart = Get-WURebootStatus -ComputerName $Name -Silent
         # Récupération Historique MAJ.
-        # $historyUpdate = getHistory($Name,1)
-        
+        $historyUpdate = getHistory($Name,1)
         # Objet à envoyer au serveur
         $toSend = @{'updates'=$result ;
         'history'=$historyUpdate;
         "needRestart"=$needRestart; 'server'= $Name}
-        # $response = Invoke-RestMethod 'http://127.0.0.1:5000/update/data' -Method 'POST' -Body ($toSend | ConvertTo-Json) -ContentType "application/json";
-        # $response
+        $response = Invoke-RestMethod "$server_url/update/data" -Method 'POST' -Body ($toSend | ConvertTo-Json) -ContentType "application/json";
+        Write-Host $response -ForeGroundColor Green
         # Etat des services
         $etat = launch_check $Name
         $dataToSend = @{'server'=$Name; 'etat'=$etat}
-        $response = Invoke-RestMethod 'http://127.0.0.1:5000/admin/data' -Method 'POST' -Body ($dataToSend | ConvertTo-Json) -ContentType "application/json";
-        $response
+        $response = Invoke-RestMethod "$server_url/admin/data" -Method 'POST' -Body ($dataToSend | ConvertTo-Json) -ContentType "application/json";
+        Write-Host $response -ForeGroundColor Green
     }
 }
 
 main
+# check_services
