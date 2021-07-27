0f4f63ea7b4fe2fa698d38e8c24ccd5568374533
Gregoire <g.guille@thirard.fr>
2021-07-27 10:29:03 +0200

    Envoi de logs à graylog et fonction obtention de tous les serveurs

---
+++ utils.ps1
@@ -0,0 +1,160 @@
+function getServers(){
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Content-Type", "multipart/form-data")
+    $postParams = @{token=$token}
+    $response = Invoke-RestMethod -Uri "$server_url/servers" -Method Post -Body $postParams;
+    return $response | ConvertTo-Json
+}
+
+enum graylog_log_type {
+    <#
+        Enum avec la correspondance des types de log
+    #>
+    info = 10
+    success = 20
+    warning = 30
+    error = 40
+    critical = 50
+}
+
+class Logger{
+    <#
+    .SYNOPSIS
+        Class pour gérer l'envoi de log a un serveur graylog pour un input http gelf.
+    .DESCRIPTION
+        Envoi de log avec différent niveau(info,success,error...) en JSON
+    .EXAMPLE
+        $logger = [Logger]::new("MonitoBot")
+    #>
+
+    [string]$application_name
+    [string]$hostname
+    [string]$graylog_url
+
+    Logger([string]$application_name){
+        <#
+        .SYNOPSIS 
+            Constructeur
+        .PARAMETER application_name
+            Nom de l'application qui est utilisé pour différencier les logs sur graylog
+        #>
+        $this.application_name = $application_name
+        $this.hostname = "$Env:COMPUTERNAME"
+        $this.graylog_url = "http://graylog.thirard.fr:12222/gelf"
+    }
+
+    Logger([string]$application_name,
+            [string]$hostname,
+            [string]$graylog_url)
+    {
+        <#
+        .SYNOPSIS 
+            Constructeur
+        .PARAMETER application_name
+            Nom de l'application qui est utilisé pour différencier les logs sur graylog
+        .PARAMETER hostname
+            Host qui envoi les logs
+        .PARAMETER graylog_url
+            URL du de l'input graylog configuré en GELF HTTP
+        #>
+        $this.application_name = $application_name
+        $this.hostname = $hostname
+        $this.graylog_url = $graylog_url
+    }
+
+    [void] send_graylog (
+            [string]$msg,
+            [graylog_log_type]$log_type
+        )
+    {
+        <#
+        .SYNOPSIS
+            Fonction d'envoi du log sur le serveur graylog
+        .PARAMETER msg
+            String
+            message qui est envoyé à graylog
+        .PARAMETER log_type
+            enum graylog_log_type
+            Type de log représenté par une valeur de l'enum graylog_log_type (info,success...)
+        .EXAMPLE
+            send_graylog("A sample message", [graylog_log_type]::success)
+        #>
+
+        # Objet powershell qui sera envoyé à graylog
+        $json = [pscustomobject]@{
+            message = $msg
+            host = $this.hostname
+            log_type = $log_type.value__
+            application = $this.application_name
+        }
+        $timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
+        Write-Host ("$($timestamp)---$($log_type)---$($msg)")
+        $response = Invoke-RestMethod $this.graylog_url -Method 'POST' -Body ($json | ConvertTo-Json) -ContentType "application/json";
+        Write-Host $response.StatusCode
+    }
+
+    [void] info([string]$msg){
+        <#
+        .SYNOPSIS
+            Envoi d'un log d'information
+        .PARAMETER msg
+            message qui est envoyé
+        .EXAMPLE
+            info("A sample info message")
+        #>
+        $this.send_graylog($msg, [graylog_log_type]::info)
+    }
+
+    [void] success([string]$msg){
+        <#
+        .SYNOPSIS
+            Envoi d'un log de succés 
+        .PARAMETER msg
+            message qui est envoyé
+        .EXAMPLE
+            info("A sample success message")
+        #>
+        $this.send_graylog($msg, [graylog_log_type]::success)
+    }
+
+    [void] warning([string]$msg){
+        <#
+        .SYNOPSIS
+            Envoi d'un log de warning
+        .PARAMETER msg
+            message qui est envoyé
+        .EXAMPLE
+            info("A sample warning message")
+        #>
+        $this.send_graylog($msg, [graylog_log_type]::warning)
+    }
+
+    [void] error([string]$msg){
+        <#
+        .SYNOPSIS
+            Envoi d'un log d'erreur
+        .PARAMETER msg
+            message qui est envoyé
+        .EXAMPLE
+            info("A sample error message")
+        #>
+        $this.send_graylog($msg, [graylog_log_type]::error)
+    }
+
+    [void] critical([string]$msg){
+        <#
+        .SYNOPSIS
+            Envoi d'un log critique
+        .PARAMETER msg
+            message qui est envoyé
+        .EXAMPLE
+            info("A sample critical message")
+        #>
+        $this.send_graylog($msg, [graylog_log_type]::critical)
+    }
+
+}
+
+
+$test = [graylog_log_type]::info
+$test.value__
