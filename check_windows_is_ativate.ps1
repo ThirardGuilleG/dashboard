7d18dcde45e41eae461cd7c4631596b70d48b27d
Gregoire <g.guille@thirard.fr>
2021-07-27 10:29:15 +0200

    debut check des serveurs activés

--- check_windows_is_ativate.ps1
+++ check_windows_is_ativate.ps1
@@ -1,3 +1,24 @@
-# Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" -ComputerName SRVT2OXO | where { $_.PartialProductKey } | select Description, LicenseStatus
-
+."$PSScriptRoot\utils.ps1"
 # cscript c:\windows\system32\slmgr.vbs -dli
+
+# logger
+$logger = [Logger]::new("Windows_Activate_Bot")
+
+function get_windows_state($computer_name){
+    $state =  Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" -ComputerName $computer_name | where { $_.PartialProductKey } | select Description, LicenseStatus;
+    return [bool]$state.LicenseStatus
+}
+
+function main(){
+    $logger.info("")
+    foreach($server in (getServers | ConvertFrom-Json)){
+        $Name,$ip = $server.value
+        $active_state = get_windows_state($Name)
+        if($active_state){
+            $logger.success("Le serveur : $($Name) est activé !")
+        }
+        else{
+            $logger.warning("Le serveur : $($Name) n'est pas activé !")
+        }
+    }
+}
