fb92e5754fbe341146baf9a4b8877b5a7ba4ce6e
Gregoire <g.guille@thirard.fr>
2021-07-28 12:04:47 +0200

    correction et envoi au serveur

--- check_windows_is_ativate.ps1
+++ check_windows_is_ativate.ps1
@@ -6,19 +6,31 @@ $logger = [Logger]::new("Windows_Activate_Bot")
 
 function get_windows_state($computer_name){
     $state =  Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" -ComputerName $computer_name | where { $_.PartialProductKey } | select Description, LicenseStatus;
-    return [bool]$state.LicenseStatus
+    if($state.LicenseStatus -eq 1){
+        return $true
+        }
+    else{
+        return $false
+        }
 }
 
 function main(){
-    $logger.info("")
+    $logger.info("Lancement de la verification de l'activations des serveurs windows")
     foreach($server in (getServers | ConvertFrom-Json)){
-        $Name,$ip = $server.value
-        $active_state = get_windows_state($Name)
+        $name,$ip = $server.value
+        $active_state = get_windows_state($name)
         if($active_state){
-            $logger.success("Le serveur : $($Name) est activé !")
+            $logger.success("Le serveur : $($name) est activé !")
         }
         else{
-            $logger.warning("Le serveur : $($Name) n'est pas activé !")
+            $logger.warning("Le serveur : $($name) n'est pas activé !")
         }
+        $logger.info("envoi de l'etat au serveur python")
+        # url pour l'update en bdd
+        $url = "http://127.0.0.1:5000/admin/active"
+        $object = @{'server'=$name;'etat'=$active_state}
+        send_json -url $url -object_to_send $object
     }
 }
+
+main
